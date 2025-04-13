294cc9964dd2ba8265394919391d85cfe7818fdc
Arjay1412 <timbancayaarjay@gmail.com>
2025-04-13 14:42:03 +0800

    Fix: Directory Re structure

--- laro_hub/BACKEND/LARO/LARO/settings.py
+++ laro_hub/BACKEND/LARO/LARO/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'LARO.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR.parent.parent.parent, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
