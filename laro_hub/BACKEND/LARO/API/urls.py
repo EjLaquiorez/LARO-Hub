9fc319e1a205c931bd605dde5980d072e0053fcc
ink3rroll <ink3rroll@gmail.com>
2025-04-30 14:44:42 +0800

    Edit: Connected myprofile.html to the database

--- laro_hub/BACKEND/LARO/API/urls.py
+++ laro_hub/BACKEND/LARO/API/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserCreate, UserRetrieveUpdateDestroy, RegisterView, LoginView, LogoutView, GameMatchView
+from .views import UserCreate, UserRetrieveUpdateDestroy, RegisterView, LoginView, LogoutView, GameMatchView, current_user_view
 
 urlpatterns = [
     path("users/", UserCreate.as_view(), name="user-create"),
+    path('current-user/', current_user_view, name='current-user'),
     path("users/<int:pk>/", UserRetrieveUpdateDestroy.as_view(), name="user-update"),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
